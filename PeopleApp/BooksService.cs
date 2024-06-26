65435382990f1253ad66ce0cd31214b1c601edfa
rvwilliams88 <112261322+rvwilliams88@users.noreply.github.com>
2024-06-26 12:59:59 +0100

    Update BooksService.cs

--- PeopleApp/BooksService.cs
+++ PeopleApp/BooksService.cs
@@ -4,18 +4,18 @@ public partial record BookRead(string? Title);
 
 public interface IBooksService
 {
-    ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson);
+    ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson, CancellationToken ct);
 }
 
 public class BooksService : IBooksService
 {
-    public async ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson)
+    public async ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson, CancellationToken ct)
     {
         List<BookRead> books = new List<BookRead>();
-        await Task.Delay(TimeSpan.FromSeconds(2));
+        await Task.Delay(TimeSpan.FromSeconds(2), ct);
         if (selectedPerson.FullName != "Jane Adams")
         {
-            books.Add(new BookRead(Title:"not Jane Adams"));
+            books.Add(new BookRead(Title: "not Jane Adams"));
         }
         else
         {
