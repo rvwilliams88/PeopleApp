4707011903525342276152635bd1d124f952f749
rvwilliams88 <richardvwilliams345@outlook.com>
2024-06-26 12:36:42 +0100

    remove Librsry

--- PeopleApp/BooksService.cs
+++ PeopleApp/BooksService.cs
@@ -4,15 +4,15 @@ public partial record BookRead(string? Title);
 
 public interface IBooksService
 {
-    ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson, CancellationToken ct);
+    ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson);
 }
 
 public class BooksService : IBooksService
 {
-    public async ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson, CancellationToken ct)
+    public async ValueTask<IImmutableList<BookRead>> GetBooksRead(Person selectedPerson)
     {
         List<BookRead> books = new List<BookRead>();
-        await Task.Delay(TimeSpan.FromSeconds(2), ct);
+        await Task.Delay(TimeSpan.FromSeconds(2));
         if (selectedPerson.FullName != "Jane Adams")
         {
             books.Add(new BookRead(Title:"not Jane Adams"));
--- PeopleApp/PeopleModel.cs
+++ PeopleApp/PeopleModel.cs
@@ -1,11 +1,8 @@
 namespace PeopleApp;
-
 public partial record PeopleModel
 {
     private IPeopleService _peopleService;
     private IBooksService _booksService;
-    public CancellationToken? _ct;
-    public Person? _selectedPerson;
 
 
     public PeopleModel(IPeopleService peopleService, IBooksService booksService)
@@ -13,23 +10,26 @@ public partial record PeopleModel
         _peopleService = peopleService;
         _booksService = booksService;
 
+
         SelectedPerson.ForEachAsync(action: SelectionChanged);
     }
 
     public async ValueTask SelectionChanged(Person? selectedPerson, CancellationToken ct)
     {
-        _ct = ct;
-        _selectedPerson = selectedPerson!;
-        if (selectedPerson == null)
-            return;
+        //_selectedPerson = selectedPerson!;
+        //if (selectedPerson == null)
+        //   return;
 
-        IImmutableList<BookRead> BooksRead = await _booksService.GetBooksRead(selectedPerson, ct);
+        IImmutableList<BookRead> BooksRead = await _booksService.GetBooksRead(selectedPerson!);
     }
     public IListFeed<Person> People => ListFeed
                                         .Async<Person>(_peopleService.GetPeople)
                                         .Selection(SelectedPerson);
 
     public IState<Person> SelectedPerson => State<Person>.Empty(this);
+   
 
-    public IListFeed<BookRead> BooksRead => ListFeed.Async<BookRead>(async (_ct) => await _booksService.GetBooksRead(_selectedPerson!, (CancellationToken)_ct));
+
+    public IListFeed<BookRead> BooksRead => ListFeed.Async<BookRead>(_booksService.GetBooksRead(SelectedPerson));
+    //public IFeed<IImmutableList<BookRead>> BooksRead => SelectedPerson.SelectAsync(<BookRead>selectedPerson => (BookRead) _booksService.GetBooksRead(selectedPerson));
 }
